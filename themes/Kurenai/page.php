cf323b21c8323851e9b9ef39dad2df092e6ed3b2
Vadims Brodskis <jamiejakov@me.com>
2021-04-19 16:02:57 +1000

    fixing up countries map

--- themes/Kurenai/page.php
+++ themes/Kurenai/page.php
@@ -8,7 +8,9 @@ get_header(); ?>
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<article role="article" class="post" id="post-<?php the_ID(); ?>">
-		<h2 class="post-title"><?php the_title(); ?></h2>
+			<div>
+				<h2 class="post-title no-link-title"><?php the_title(); ?></h2>
+			</div>
 			<div class="entry">
 				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
